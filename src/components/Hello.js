8cb4eb1d5ed7528f2b90c79b7376512ced3a8c68
James Loewen <jamesfloewen@gmail.com>
2023-04-12 14:39:33 -0400

    Implement basic light/dark theme boilerplate

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,19 @@
 import './styles.scss';
 
 export function Hello(props) {
-  return <h1>Hello from React!</h1>;
+  const toggleTheme = () => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.toggle('light-theme');
+    } else {
+      document.body.classList.toggle('dark-theme');
+    }
+  }
+  return (
+    <div className="card">
+      <h1 class="hello">Hello from React!</h1>
+      <p>Tryin' out some different styling/theming techniques!</p>
+      <button className='theme-toggle-btn'
+              onClick={toggleTheme}>toggle theme</button>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
+import './scss/_main.scss';
 import { createRoot } from 'react-dom/client';
 import { Hello } from './components/Hello';
 
-const helloDiv = document.createElement('div');
-helloDiv.innerHTML = 'Hello from Javascript!';
-document.body.appendChild(helloDiv);
-
-const container = document.querySelector('#root');
-const root = createRoot(container);
+const root = createRoot(document.querySelector('#root'));
 root.render(<Hello />);
