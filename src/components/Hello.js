e27951299e082dcd4f8551c9892976bed5541313
James Loewen <jamesfloewen@gmail.com>
2023-04-18 20:49:31 -0400

    Remove CSS theme media query (use js instead)

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Hello(props) {
   const [theme, setTheme] = useState(
     window.matchMedia('(prefers-color-scheme: dark') ? 'light' : 'dark'
   );
 
-  const toggleTheme = () => {
+  useEffect(() => {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.classList.toggle('light-theme');
-      setTheme(theme === 'dark' ? 'light' : 'dark');
+      document.body.classList.add('dark-theme');
     } else {
-      document.body.classList.toggle('dark-theme');
-      setTheme(theme === 'dark' ? 'light' : 'dark');
+      document.body.classList.add('light-theme');
     }
+  }, []);
+
+  const toggleTheme = () => {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   }
 
   return (
@@ -20,7 +24,7 @@ export function Hello(props) {
       <h1 className="hello">Hello from React!</h1>
       <p>Tryin' out some different styling & theming techniques!</p>
       <button className='theme-toggle-btn'
-              onClick={toggleTheme}>{theme} theme</button>
+              onClick={toggleTheme}>{theme === 'dark' ? 'light' : 'dark'} theme</button>
     </div>
   );
 }
